85e06d88cba40682439ec0cdc2f2c75dfff9b522
Imersad <imersad94@gmail.com>
2021-01-19 18:27:18 +0100

    Wrap the entire app inside a Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import { Provider } from '../context';
 
 // pages/_app.js
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider>
       <Nav />
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
